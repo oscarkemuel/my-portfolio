64a265220eaaf16171bd5d9b4f1a43bdc9e673ac
Oscar Kemuel <oscar.kemuel5@gmail.com>
2023-06-04 02:05:55 -0300

    feat: finalize header with navbar and final adjustments of responsiveness

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useEffect } from "react";
+import styles from "./styles.module.scss";
+import { IoClose } from "react-icons/io5";
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+  background?: string;
+  title?: string;
+  width?: string;
+}
+
+export function Drawer({ open, onClose, children, background, title, width }: Props) {
+  function staticBody() {
+    document.body.style.left = "0";
+    document.body.style.position = "fixed";
+    document.body.style.top = "0";
+    document.body.style.width = "100%";
+  }
+
+  function autoBody() {
+    document.body.style.left = "auto";
+    document.body.style.position = "static";
+    document.body.style.top = "auto";
+    document.body.style.width = "auto";
+  }
+
+  useEffect(() => {
+    if (open) {
+      staticBody();
+    } else {
+      autoBody();
+    }
+  }, [open]);
+
+  return (
+    <div
+      className={`${styles.drawerContainer} ${
+        open ? styles.drawerContainerIsOpen : null
+      }`}
+      style={{
+        background,
+        width,
+      }}
+    >
+      <div className={styles.header}>
+        <h1>{title}</h1>
+
+        <button type="button" onClick={onClose}>
+          <IoClose size={20} />
+        </button>
+      </div>
+
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+}
+
+Drawer.defaultProps = {
+  width: "100vw",
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { useState } from "react";
+import { Drawer } from "../Drawer";
 import styles from "./styles.module.scss";
+import { FaBars } from "react-icons/fa";
 
 export function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleShow = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsDrawerOpen(false);
+  };
+
   const sections = [
     { name: "Sobre", id: "about" },
     { name: "Projetos", id: "projects" },
@@ -11,21 +24,30 @@ export function Navbar() {
 
   return (
     <>
-      <nav className={styles.container}>
+      <header className={styles.container}>
         <a href="#home">
           <h1>Oscar Kemuel</h1>
         </a>
 
-        <ul className={styles.links}>
-          {sections.map((section, index) => {
-            return (
-              <li key={index}>
-                <a href={`#${section.id}`}>{section.name}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+        <nav>
+          <ul className={styles.links}>
+            {sections.map((section, index) => {
+              return (
+                <li key={index}>
+                  <a href={`#${section.id}`} id={`To #${section.id}`}>
+                    {section.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <button type="button" onClick={handleShow}>
+          <FaBars size={20} />
+        </button>
+      </header>
+
       <hr
         style={{
           width: "100%",
@@ -34,6 +56,31 @@ export function Navbar() {
           borderRadius: "0",
         }}
       />
+
+      <Drawer
+        open={isDrawerOpen}
+        onClose={handleClose}
+        background="var(--background-reverse)"
+        title="Oscar Kemuel"
+      >
+        <nav>
+          <ul className={styles.contentDrawer}>
+            {sections.map((section, index) => {
+              return (
+                <li key={index}>
+                  <a
+                    href={`#${section.id}`}
+                    id={`To #${section.id}`}
+                    onClick={handleClose}
+                  >
+                    {section.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </Drawer>
     </>
   );
 }
