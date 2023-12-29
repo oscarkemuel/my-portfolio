f77dc80a6e34d0078218670988d33baf7d7971c5
Oscar Kemuel <oscar.kemuel5@gmail.com>
2023-12-29 04:17:52 -0300

    fix(drawer): change styles

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
 import styles from "./styles.module.scss";
 import { IoClose } from "react-icons/io5";
 import Link from "next/link";
@@ -18,28 +17,6 @@ interface Props {
 
 export function Drawer({ open, onClose, children, background, title, width = '100vw', linkTitle }: Props) {
   const { back } = useRouter();
-
-  function staticBody() {
-    document.body.style.left = "0";
-    document.body.style.position = "fixed";
-    document.body.style.top = "0";
-    document.body.style.width = "100%";
-  }
-
-  function autoBody() {
-    document.body.style.left = "auto";
-    document.body.style.position = "static";
-    document.body.style.top = "auto";
-    document.body.style.width = "auto";
-  }
-
-  useEffect(() => {
-    if (open) {
-      staticBody();
-    } else {
-      autoBody();
-    }
-  }, [open]);
 
   return (
     <div
