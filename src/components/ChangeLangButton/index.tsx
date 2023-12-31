c0794074917b761edf17c24102565aba8659d876
Oscar Kemuel <oscar.kemuel5@gmail.com>
2023-12-30 23:25:19 -0300

    feat(lang): changue toggle language

--- src/components/ChangeLangButton/index.tsx
+++ src/components/ChangeLangButton/index.tsx
@@ -2,39 +2,52 @@
 
 import { usePathname, useRouter } from "@/navigation";
 import styles from "./styles.module.scss";
+import flag_br from "../../../public/flag_br.svg";
+import flag_us from "../../../public/flag_us.svg";
+import Image from "next/image";
 
-export function ChangeLangButton() {
+interface Props {
+  locale: string;
+}
+
+export function ChangeLangButton({ locale }: Props) {
   const pathname = usePathname();
   const router = useRouter();
 
-  const langs = [
-    {
-      icon: "🇧🇷",
+  const getNewLocale = () => {
+    if (locale === "pt-BR") {
+      return {
+        locale: "en-US",
+        icon: flag_us,
+      };
+    }
+
+    return {
       locale: "pt-BR",
-    },
-    {
-      icon: "🇺🇸",
-      locale: "en-US",
-    },
-  ];
+      icon: flag_br,
+    };
+  };
+
+  const newLocale = getNewLocale();
 
   return (
     <div className={styles.container}>
-      {langs.map((lang, index) => {
-        return (
-          <button
-            key={index}
-            onClick={() => {
-              router.push(pathname, {
-                locale: lang.locale,
-              });
-              router.refresh();
-            }}
-          >
-            {lang.icon}
-          </button>
-        );
-      })}
+      <button
+        key={newLocale.locale}
+        onClick={() => {
+          router.push(pathname, {
+            locale: newLocale.locale,
+          });
+          router.refresh();
+        }}
+      >
+        <Image
+          src={newLocale.icon}
+          alt="flag to change language"
+          width={35}
+          height={45}
+        />
+      </button>
     </div>
   );
 }
