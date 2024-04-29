b22a40813582ca6008a9e980e24e11b1e6ce3310
Oscar Kemuel <oscar.kemuel5@gmail.com>
2024-04-29 00:37:32 -0300

    feat(sitemaps): add blog posts in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,31 +1,47 @@
-import { constants } from '@/utils/constants';
-import { MetadataRoute } from 'next'
+import { IPost } from "@/services/contentful/types";
+import { constants } from "@/utils/constants";
+import { MetadataRoute } from "next";
 
-function generatePages() {
+async function generatePages() {
   const defaultUrl = constants.defaultUrl;
 
-  const pagesPaths = [
-    'about',
-    'contact',
-    'projects',
-    'blog'
-  ]
-  
+  const pagesPaths = ["about", "contact", "projects"];
+
+  const responseBlogPosts = await fetch(
+    `${process.env.SITE_URL || defaultUrl}/api/blog`
+  );
+  const data = await responseBlogPosts.json();
+  const blogPosts: IPost[] = data.posts;
+
   const pages = [
     {
       url: `${process.env.SITE_URL || defaultUrl}`,
       lastModified: new Date(),
       priority: 1,
+      changeFrequency: 'yearly'
     },
     ...pagesPaths.map((path) => ({
       url: `${process.env.SITE_URL || defaultUrl}/${path}`,
       lastModified: new Date(),
     })),
-  ]
+    {
+      url: `${process.env.SITE_URL || defaultUrl}/blog`,
+      lastModified: new Date(),
+      priority: 1,
+      changeFrequency: 'monthly'
+    },
+    ...blogPosts.map((post) => ({
+      url: `${process.env.SITE_URL || defaultUrl}/blog/${post.slug}`,
+      lastModified: new Date(post.updatedAt),
+      priority: 1,
+    })),
+  ];
 
-  return pages
+  return pages;
 }
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [...generatePages()]
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pages = await generatePages() as MetadataRoute.Sitemap;
+
+  return [...pages];
 }
